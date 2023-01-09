1d5d33edb2c5ea226b48960294da94a1c556b67e
Eric Frey <ericlfrey@gmail.com>
2023-01-09 16:31:44 -0600

    Finished the Lab

--- pages/counter/index.js
+++ pages/counter/index.js
@@ -3,10 +3,17 @@ import Counter from '../../components/Counter';
 import styles from './counter.module.css';
 
 export default function CounterPage() {
+  const counters = [
+    { title: 'Counter 1' },
+    { title: 'Counter 2' },
+    { title: 'Counter 3' },
+  ];
   return (
     <div className={styles.center}>
       <h1 className={styles.headingMargin}>Counter Page</h1>
-      <Counter />
+      {counters.map((counter) => (
+        <Counter key={counter.title} title={counter.title} />
+      ))}
     </div>
   );
 }
